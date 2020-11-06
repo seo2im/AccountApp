5541ba17aaf8f2818bdd7cc65f9d076455c4f684
seo2im <seo2im6492@gmail.com>
2020-11-06 15:38:31 +0900

    init app

---
+++ AccoutApp/srcs/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  View
+} from 'react-native';
+
+
+/* const App: () => React$Node = () => { */
+const App = () => {
+  return (
+    <View>
+
+    </View>
+  )
+};
+
+
+export default App;
